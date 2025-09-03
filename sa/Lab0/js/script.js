6ba989498ab03cee7afb762d6948f20a7cfa2a50
Jayden <jaydenhutchinson747@gmail.com>
2025-09-02 19:37:12 -0700

    init

---
+++ sa/Lab0/js/script.js
@@ -0,0 +1,25 @@
+class ColourButton extends HTMLButtonElement {
+    constructor(index, colour, width, height, top, left) {
+        super();
+
+        this.index = index;
+        this.style.color = colour;
+        this.style.width = width;
+        this.style.height = height;
+        this.style.position = "absolute";
+        this.style.top = top;
+        this.style.left = left;
+    }
+
+    setPosition(top, left) {
+        this.style.top = top;
+        this.style.left = left;
+    }
+}
+
+customElements.define("colour-button", ColourButton, { extends: "button" })
+
+let colourButtons = [];
+
+buttons.push(new ColourButton("Red"))
+buttons.push(new ColourButton())
