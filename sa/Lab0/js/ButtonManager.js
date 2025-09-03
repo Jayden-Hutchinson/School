f1ac63c3fd1879ec4ba110469876d9bd315f26af
Jayden H <jaydenhutchinson747@gmail.com>
2025-09-02 20:34:03 -0700

    push

--- sa/Lab0/js/ButtonManager.js
+++ sa/Lab0/js/ButtonManager.js
@@ -1,3 +1,9 @@
+const goButton = "go-button";
+
 class ButtonManager {
+  goButton = document.getElementById(goButton);
   colourButtons = [];
 }
+
+const buttonManager = new ButtonManager();
+console.log(buttonManager.goButton);
