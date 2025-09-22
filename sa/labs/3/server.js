58e6704086970755b7cfc376f442d92695d2fdae
Jayden H <jaydenhutchinson747@gmail.com>
2025-09-22 09:24:37 -0700

    push

--- sa/labs/3/server.js
+++ sa/labs/3/server.js
@@ -1,4 +1,14 @@
-import http from "http";
-import { URL } from "url";
+const http = require("http");
+const { URL } = require("url");
 
-const server = http.createServer((req, res) => {});
+const server = http.createServer((req, res) => {
+  const url = new URL(req.url, `https://${req.headers.host}`);
+  const name = url.searchParams.get("name") || "Guest";
+
+  res.send(`<p>hello<p/>`);
+});
+
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
