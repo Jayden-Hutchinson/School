66290e9a01318bb9eb792ade67068fa089570d38
Jayden H <jaydenhutchinson747@gmail.com>
2025-09-22 19:47:18 -0700

    push

--- sa/labs/3/server.js
+++ sa/labs/3/server.js
@@ -1,11 +1,20 @@
-const http = require("http");
+const https = require("https");
+const fs = require("fs");
 const { URL } = require("url");
 
-const server = http.createServer((req, res) => {
+const options = {
+  key: fs.readFileSync("server.key"),
+  cert: fs.readFileSync("server.cert"),
+};
+
+const server = https.createServer(options, (req, res) => {
   const url = new URL(req.url, `https://${req.headers.host}`);
   const name = url.searchParams.get("name") || "Guest";
 
-  res.send(`<p>hello<p/>`);
+  res.writeHead(200, { "Content-Type": "text/html; color: blue" });
+  res.end(
+    `<h1 style="color:blue">Hello, ${name}!<br>Server time: ${new Date().toLocaleString()}</h1>`
+  );
 });
 
 const PORT = 3000;
