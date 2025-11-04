b6531648b80a29fbb1c7b0ae26c650c08baa9375
Jayden <jaydenhutchinson747@gmail.com>
2025-11-04 15:03:28 -0800

    push

---
+++ cp/lab6/pqr.go
@@ -0,0 +1,12 @@
+package lab6
+
+// Consider 3 goroutines, one keeps printing P's, another keeps printing Q's
+// & the third keeps printing R's. Use mutexes and condition variables to
+// synchronize the 3 goroutines so that at all times
+//
+// number of P's printed <= number of Q's printed <= number of R's printed
+//
+// Use comments to explain what you are doing.
+func pqr() {
+
+}
---
+++ cp/lab6/primes.go
@@ -0,0 +1,17 @@
+package lab6
+
+// Write a Go program to find the size of the largets set of 6-digit primes
+// that are permutations of one another.
+//
+// Return a slice containing all prmes p satisfying m <= p < n.
+// The implementation must be based on the Sieve of Eratosthenes.
+// For testing purposes, your program must print out the size of the set of
+// all 6-digit primes as well as the size of the largest set of 6-digit
+// primes that are permutations of one another.
+// Do not print out eh set of all 6-digit primes - you only need to print
+// its size.
+
+// precondition: 0 <= m < n
+func primes(m int, n int) []int {
+	return []int{}
+}
